ce8a35da68297a7d600208128527c3bb69af9b4f
Brandon <403493@gllm.ac.uk>
2025-12-08 21:11:27 +0000

    adding new connectFour Files

--- script.js
+++ script.js
@@ -1,69 +1,39 @@
-// JavaScript Document
-let turn = 0;
-setUpGrid();
+let mainDiv = document.querySelector("#mainDiv")
+let purple = "rgb(110, 14, 73)", yellow = "rgb(250, 250, 152)";
 
-function setUpGrid() {
-	const mainBox = document.querySelector("#mainBox");
-	const choiceBox = document.querySelector("#chooseBox");
-	
+setBoard();
 
-	
-	for(let y = 0; y <= 7; y++) {//column creation
-		
-		let col = document.createElement("div");
-		
-		col.setAttribute("class","column");
-		
-		mainBox.appendChild(col);
+function setBoard() {
+    for(let y = 0; y < 6; y++) {
+        let curCol = document.createElement("div");
 
-		for(let i = 0; i <= 6; i++) {//circle creation
-			
-			let circ = document.createElement("div");
-			
-			circ.setAttribute("class","circle");
-			circ.setAttribute("id", `c${i}-${y}`);
-			
-			col.appendChild(circ);
-		}
-	}
-	
-	for(let u = 0; u <=7; u++) {//choice circle creation
-		
-		let choice = document.createElement("div");
-		
-		choice.setAttribute("class", "choiceCircle");
-		choice.setAttribute("id", `choice${u}`);
-		
-		choice.addEventListener("click",() => {
-			addPiece(u);
-		});
-		
-		choiceBox.appendChild(choice);
-	}
-	
+        curCol.setAttribute("class", "column");
+        curCol.setAttribute("id", `column${y}`);
+
+        for(let x = 0; x < 7; x++) {
+            let curCirc = document.createElement("div");
+            let circCoord = `c${y}-${x}`;//column - row
+
+            curCirc.setAttribute("class", "circle");
+            curCirc.setAttribute("id", circCoord);
+
+            curCirc.addEventListener("click", () => {
+                if(getBodyBackground() == yellow) {
+                    document.body.style.backgroundColor = purple;
+                }
+                else {
+                    document.body.style.backgroundColor = yellow;
+                }
+            });
+            curCol.appendChild(curCirc);
+        }
+        mainDiv.appendChild(curCol);
+    }
 }
 
-function addPiece(choiceNum) {
-	const player1 = "red", player2 = "yellow";
+function getBodyBackground() {
+    var getBody = document.getElementsByTagName("body")[0]
+    var prop = window.getComputedStyle(getBody).getPropertyValue("background-color");
 
-
-	for(let i = 6; i >= 0; i--) {
-		let cur = document.querySelector(`#c${i}-${choiceNum}`);
-
-		console.log(cur.style.backgroundColor);
-		if(cur.style.backgroundColor == "aliceblue") {
-			if(turn % 2 == 0) {
-				cur.style.backgroundColor = player2;
-			}
-			else {
-				cur.style.backgroundColor = player1;
-			}
-			
-		}
-	}
-	turn++;
+    return prop;
 }
-  
-//function colCount(column) {
-//		
-//}
